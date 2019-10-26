93d952bc7816b29a15766fb25898cac6b88afd8a
timm0e <11885527+timm0e@users.noreply.github.com>
2019-10-26 18:03:38 +0200

    force RPI3

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import os
+
+# Force RPI3 since it isn't detected automatically on ubuntu
+os.environ["BLINKA_FORCECHIP"] = "BCM2XXX"
+os.environ["BLINKA_FORCEBOARD"] = "RASPBERRY_PI_3B"
+
 from pykeepass import PyKeePass
 
 from pi.rotaryEncoder import RotaryInput
