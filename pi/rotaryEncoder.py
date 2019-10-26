625a1e600d687adb028d67555d2b2d2378cf0cae
timm0e <11885527+timm0e@users.noreply.github.com>
2019-10-26 16:52:40 +0200

    implement callback system

--- pi/rotaryEncoder.py
+++ pi/rotaryEncoder.py
@@ -1,5 +1,5 @@
-from RPi import GPIO
 from time import sleep
+from RPi import GPIO
 
 clk = 2
 dt = 3
@@ -11,33 +11,51 @@ GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-counter = 0
-clkLastState = GPIO.input(clk)
-swLastState = 0
-try:
-    while True:
-        clkState = GPIO.input(clk)
 
-        if clkState != clkLastState:
-            dtState = GPIO.input(dt)
-            if dtState != clkState:
-                counter -= 1
-                if counter % 2 == 0:
-                        print("prev")
-            else:
-                if counter % 2 == 1:
-                        print("next")
-                counter += 1
-        clkLastState = clkState
+class Callback:
+    def next(self):
+        pass
 
-        swState = GPIO.input(sw)
-        if swLastState != swState:
-            swLastState = swState
-            if swState == 0:
-                print("down")
+    def prev(self):
+        pass
 
-        sleep(0.001)
-finally:
-    GPIO.cleanup()
+    def press(self):
+        pass
 
 
+class RotaryInput:
+    def __init__(self, callback):
+        self.callback = callback
+        self.counter = 0
+        self.clkLastState = GPIO.input(clk)
+        self.swLastState = 0
+        self.dtState = None
+        self.clkState = None
+        self.swState = None
+
+    def run(self):
+        try:
+            while True:
+                self.clkState = GPIO.input(clk)
+
+                if self.clkState != self.clkLastState:
+                    self.dtState = GPIO.input(dt)
+                    if self.dtState != self.clkState:
+                        self.counter -= 1
+                        if self.counter % 2 == 0:
+                            self.callback.prev()
+                    else:
+                        if self.counter % 2 == 1:
+                            self.callback.next()
+                        self.counter += 1
+                self.clkLastState = self.clkState
+
+                self.swState = GPIO.input(sw)
+                if self.swLastState != self.swState:
+                    self.swLastState = self.swState
+                    if self.swState == 0:
+                        self.callback.press()
+
+                sleep(0.001)
+        finally:
+            GPIO.cleanup()
